471c3a8eba67d297598a7c86973f47d8aa7619be
OlivandoGit <oliver.lister2002@gmail.com>
2022-12-06 13:07:55 +0000

    Completed Day 06

---
+++ Day 06/Part 2.py
@@ -0,0 +1,19 @@
+with open("Input.txt", "r") as file:
+    line = file.read().strip()
+
+    group = []
+    for i, char in enumerate(line):
+        if len(group) < 4:
+            group.append(char)
+
+        if len(group) == 4:
+            found = True
+            for char2 in group:
+                if group.count(char2) > 1:
+                    found = False
+
+            if found:
+                print(i + 1)
+                break
+
+            group = group[1:]
